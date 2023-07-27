d600c7976d3eba444c5a4a1714d5c06c44b17673
Goraniya Karan <karangoraniya@gmail.com>
2023-07-27 08:27:32 +0530

    feat: add images and logo 📷

---
+++ app/assets/index.ts
@@ -0,0 +1,17 @@
+import arbitrum from './arbitrum.svg';
+import avalanche from './avalanche.svg';
+import nft from './nft.png';
+import Phantom from './Phantom.png';
+import MagicEden from './MagicEden.jpeg';
+import Polygon from './Polygon.jpeg';
+import Solana from './Solana.jpeg';
+
+export default {
+  arbitrum,
+  avalanche,
+  nft,
+  Phantom,
+  MagicEden,
+  Polygon,
+  Solana,
+};
