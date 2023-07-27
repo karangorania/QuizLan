1089e8070894e035d64d7b28effb9b27d1679e73
jahnvi-vyas <107184591+jahnvi-vyas@users.noreply.github.com>
2023-07-27 11:55:52 +0530

    feat: Add error and other pages

---
+++ app/quiz/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+
+const Quiz = () => {
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  function ClaimBadge() {
+    return <div>Success</div>;
+  }
+
+  function QuizQuens() {
+    return (
+      <>
+        <div className="quiz-quen-wrapper">
+          <h2 className="quen-header">ONE-WORD ASSOCIATION WITH ALEO</h2>
+          <div className="quen-options-wrapper">
+            <button className="quen-options">privacy</button>
+            <button className="quen-options">transparency</button>
+            <button className="quen-options">scalability</button>
+          </div>
+        </div>
+        <div className="quen-footer">
+          <button onClick={() => setIsSuccess(true)}>Success</button>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <div className="quiz-detail-wrapper">
+      <div className="quiz-back-wrapper">
+        <Link href="/course">
+          <p>&#x2190; Arbitrum</p>
+        </Link>
+      </div>
+      <div className="quiz-content-wrapper">
+        <p className="content-title">
+          {isSuccess ? "CLAIM BADGE" : "SIMPLE QUIZ"}
+        </p>
+        {isSuccess ? <ClaimBadge /> : <QuizQuens />}
+      </div>
+    </div>
+  );
+};
+
+export default Quiz;
