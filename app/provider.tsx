491b513a01345a3414b0437824315212adeaccc8
jahnvi-vyas <107184591+jahnvi-vyas@users.noreply.github.com>
2023-07-27 11:59:45 +0530

    refactor: remove unnecessary code

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,23 +1,23 @@
-'use client';
-import React, { FC, useMemo, ReactNode } from 'react';
+"use client";
+import React, { FC, useMemo, ReactNode } from "react";
 import {
   ConnectionProvider,
   WalletProvider,
-} from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import {
   PhantomWalletAdapter,
   UnsafeBurnerWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
+} from "@solana/wallet-adapter-wallets";
 import {
   WalletModalProvider,
   WalletDisconnectButton,
   WalletMultiButton,
-} from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
+} from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
 
 // Default styles that can be overridden by your app
-require('@solana/wallet-adapter-react-ui/styles.css');
+require("@solana/wallet-adapter-react-ui/styles.css");
 
 interface WalletProps {
   children: ReactNode;
@@ -40,8 +40,6 @@ export const Wallet: FC<WalletProps> = ({ children }) => {
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>
-          <WalletMultiButton />
-          <WalletDisconnectButton />
           {children} {/* This line will render the children */}
         </WalletModalProvider>
       </WalletProvider>
