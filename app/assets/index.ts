c8b293b2c50ec472cfb847edf4673376e36b4129
jahnvi-vyas <107184591+jahnvi-vyas@users.noreply.github.com>
2023-07-31 17:57:50 +0530

    refactor: modify assets file

--- app/assets/index.ts
+++ app/assets/index.ts
@@ -1,20 +1,16 @@
-import arbitrum from './arbitrum.svg';
-import avalanche from './avalanche.svg';
 import nft from './nft.png';
 import Phantom from './Phantom.png';
-import MagicEden from './MagicEden.jpeg';
-import Polygon from './Polygon.jpeg';
 import Solana from './Solana.jpeg';
-import sol from './solana.png';
+import Encode from './encode.jpeg';
+import BlockChain from './blockchain.png';
+import NAS from './nas.png';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  arbitrum,
-  avalanche,
   nft,
   Phantom,
-  MagicEden,
-  Polygon,
   Solana,
-  sol,
+  NAS,
+  Encode,
+  BlockChain
 };
